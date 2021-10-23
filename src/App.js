7f4a2575098620c674bac482fec883c10581ccab
sudhir yadav <sudhir9297@gmail.com>
2021-10-23 15:36:29 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { ToggleButton } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <ToggleButton />
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/components/ToggleButton/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ToggleButton() {
+  return <div>Button</div>;
+}
+
+export default ToggleButton;
